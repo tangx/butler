4c9d3fca1b8c8c656d83d050120d2e0cc6231ee7
tangx <uyinn@live.com>
2021-06-24 10:43:40 +0800

    使用信号安全退出
    * feat: signal
    
    * feat: signal concel done

--- __examples__/main.go
+++ __examples__/main.go
@@ -1,18 +1,19 @@
-package butler
+package main
 
 import (
 	"fmt"
 	"log"
 	"math/rand"
-	"testing"
 	"time"
+
+	"github.com/tangx/butler"
 )
 
-func Test_Butler(t *testing.T) {
+func main() {
 	// b := Defualt()
 
-	b := &Butler{}
-	b.Init(WithJobs(20), WithWorkers(5))
+	b := &butler.Butler{}
+	b.Init(butler.WithJobs(20), butler.WithWorkers(15))
 
 	go func() {
 		for i := 0; i < 30; i++ {
@@ -32,7 +33,11 @@ func newJob() func() {
 		rand.Seed(time.Now().UnixNano())
 		t := rand.Intn(5)
 		time.Sleep(time.Duration(t) * time.Second)
-		fmt.Printf("job %d: sleep %d \n", rand.Int(), t)
+		jobid := rand.Int()
+		fmt.Printf("job %d: sleep %d \n", jobid, t)
+		if t%2 == 0 {
+			log.Panic(jobid)
+		}
 	}
 
 }
--- butler.go
+++ butler.go
@@ -1,8 +1,13 @@
 package butler
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
 	"runtime"
+	"sync"
+	"syscall"
 )
 
 type Butler struct {
@@ -10,6 +15,9 @@ type Butler struct {
 	workerQueue chan *worker
 	jobs        int
 	jobQueue    chan func()
+
+	ctx context.Context
+	wg  sync.WaitGroup
 }
 
 // Default return a butler , which workers' number is equal GOMAXPROC
@@ -17,7 +25,7 @@ type Butler struct {
 // https://golang.org/doc/effective_go#parallel
 func Default() *Butler {
 	b := &Butler{}
-	b.Init()
+	b.initial()
 	return b
 }
 
@@ -27,17 +35,36 @@ func (b *Butler) Init(funcs ...OptionFunc) {
 		fn(b)
 	}
 
-	b.init()
+	b.initial()
 }
 
 // Work start
 func (b *Butler) Work() {
+	// https://colobu.com/2015/10/09/Linux-Signals/
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	for worker := range b.workerQueue {
-		job := <-b.jobQueue
-		log.Println(">>> a worker accpet a new job")
-		go b.assign(worker, job)
+Loop:
+	for {
+		select {
+		// catch a signal and break out loop
+		case sig := <-sigs:
+			log.Printf(">>>>>>>> catch signal %v \n", sig)
+			break Loop
+		case worker := <-b.workerQueue:
+			select {
+			case job := <-b.jobQueue:
+				go b.assign(worker, job)
+			default:
+				// if no jobs, return into worker queue
+				b.workerQueue <- worker
+			}
+		}
 	}
+
+	// wait all jobs done
+	b.wg.Wait()
+
 }
 
 func (b *Butler) AddJobs(funcs ...func()) {
@@ -48,27 +75,35 @@ func (b *Butler) AddJobs(funcs ...func()) {
 
 // SetDefaults set default value for butler
 func (b *Butler) SetDefaults() {
-	b.workers = runtime.GOMAXPROCS(0)
+	if b.workers < 1 {
+		b.workers = runtime.GOMAXPROCS(0)
+	}
+
 	b.jobs = b.workers * 2
 }
 
-// init butler
-func (b *Butler) init() {
+// initial butler
+func (b *Butler) initial() {
 	b.SetDefaults()
 
 	b.jobQueue = make(chan func(), b.jobs)
+
 	b.workerQueue = make(chan *worker, b.workers)
 	for i := 0; i < b.workers; i++ {
 		log.Println("register a new worker")
 		b.workerQueue <- newWorker()
 	}
+
 }
 
 // assign a job to a worker
 func (b *Butler) assign(w *worker, job func()) {
+	b.wg.Add(1)
+	defer func() { b.wg.Done() }()
+
 	defer func() {
 		if err := recover(); err != nil {
-			log.Println(err)
+			log.Printf("catch panic: %v", err)
 		}
 
 		b.workerQueue <- w
